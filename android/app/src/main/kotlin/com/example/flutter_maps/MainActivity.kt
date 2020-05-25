a11cf57374c5d5a948cbf0ee5c9c8197b9ec80f0
Souvik Biswas <sbis1999@gmail.com>
2020-05-25 12:41:10 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_maps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_maps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+import Flutter
+import GoogleMaps
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Add your Google Maps API Key here
+    GMSServices.provideAPIKey("YOUR KEY HERE")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
