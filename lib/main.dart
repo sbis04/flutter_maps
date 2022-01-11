23fc7a14ad3f82dc29bb5266512b6b2b2e2cbbe6
Jacob Okello Okomo <jaylabs2030@gmail.com>
2022-01-11 17:57:59 +0300

    Disabled the debug Mode Banner in line 18
    ```jsx
    debugShowCheckedModeBanner: false,
    ```

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Maps',
       theme: ThemeData(
         primarySwatch: Colors.blue,
